3f4b1d041ba09aec384903d5e8512778d7117f14
Antonio Kim <antoniok9130@gmail.com>
2019-04-15 10:31:29 -0400

    Implemented Divide and Conquer Binary to Decimal

---
+++ Algorithms/Divide and Conquer/Bin to Dec/binToDec.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def binToDec(b):
+    n = len(b)
+    if (n == 1):
+        return b[0]
+    
+    L = binToDec(b[:n//2])
+    R = binToDec(b[n//2:])
+    
+    return L*2**(n-n//2)+R
+
+def test_binToDec(b):
+    expected = int("".join(map(str, b)), 2)
+    result = binToDec(b)
+    if (result != expected):
+        raise Exception(f"{result} != {expected}")
+#     else:
+#         print(f"{result} == {expected}")
+
+if __name__ == "__main__":
+    for i in range(10000):
+        test_binToDec(list(np.random.randint(2, size=np.random.randint(10, 50))))
+        
+    print("All Divide and Conquer Binary to Decimal Tests Passed!")
